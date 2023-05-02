0bc2352aa7570e556db824bd1ce7ca384e6c291c
michelle nderi <nderimichelle@gmail.com>
2023-05-02 15:03:16 +0300

    Get request for questions

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,12 @@
 import React from "react";
-
-function QuestionList() {
+import QuestionItem from "./QuestionItem";
+function QuestionList({questions}) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question)=>(
+     <QuestionItem key={question.id} question={question} />
+      ))}</ul>
     </section>
   );
 }
